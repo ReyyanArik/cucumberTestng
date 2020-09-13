2c8b229d1778b396a58ad87a2d38fc19bbffa5dc
Reyyan ARIK <reyyan.arik@nesine.com>
2020-09-13 12:50:23 +0300

    delete project

--- src/test/java/ConstantVariables/Constant.java
+++
@@ -1,6 +0,0 @@
-package ConstantVariables;
-public class Constant {
-	public static final String glue = "stepdefs";
-	public static final String tags = "@home_page";
-
-}
--- src/test/resources/features/01_VerifyPageName_BlogPage.feature
+++
@@ -1,5 +0,0 @@
-Feature: UITest Scenarios
-
-  @home_page
-  Scenario: UITest
-    Given User navigates to url "https://www.google.com/"
